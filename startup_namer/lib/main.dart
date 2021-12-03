e4af329c41abf4aa278a0c95fcd298a0e94c2b5e
Nguyễn Lê Bảo Thi <66853454+baothi250501@users.noreply.github.com>
2021-12-03 15:19:19 +0700

    Final 1.5 Add a stateful widget

--- startup_namer/lib/main.dart
+++ startup_namer/lib/main.dart
@@ -3,6 +3,18 @@ import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());
 
+class _RandomWordsState extends State<RandomWords>{
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+    return Text(wordPair.asPascalCase);
+  }
+  
+}
+class RandomWords extends StatefulWidget{
+  @override
+  _RandomWordsState createState() => _RandomWordsState();
+}
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -14,8 +26,7 @@ class MyApp extends StatelessWidget {
           title: const Text('Welcome to Flutter'),
         ),
         body: Center(
-          //child: Text('Hello World'),
-          child: Text(wordPair.asPascalCase),
+          child: RandomWords(),
         ),
       ),
     );
