8828ae96598188bf1597640012e5b3a3e5f19e7b
Nguyễn Lê Bảo Thi <66853454+baothi250501@users.noreply.github.com>
2021-12-03 15:51:50 +0700

    Final 2.5 Add interactivity

--- startup_namer/lib/main.dart
+++ startup_namer/lib/main.dart
@@ -91,6 +91,16 @@ class _RandomWordsState extends State<RandomWords>{
         color: alreadySaved ? Colors.red: null,
         semanticLabel: alreadySaved ? 'Remove from saved' : 'Save',
       ),
+      onTap: () {
+        //Notify the framework that state has changed
+        setState(() {
+          if (alreadySaved){
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+      }
     );
   }
 
