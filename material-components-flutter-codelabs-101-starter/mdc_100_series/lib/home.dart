fffd691f561dd1efda2afe45ff4b8ecee6ad1929
Nguyễn Lê Bảo Thi <66853454+baothi250501@users.noreply.github.com>
2021-12-06 00:07:43 +0700

    Final 2.6 Make a card collection

--- material-components-flutter-codelabs-101-starter/mdc_100_series/lib/home.dart
+++ material-components-flutter-codelabs-101-starter/mdc_100_series/lib/home.dart
@@ -13,6 +13,9 @@
 // limitations under the License.
 
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:shrine/model/product.dart';
+import 'package:shrine/model/products_repository.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -62,34 +65,63 @@ class HomePage extends StatelessWidget {
         crossAxisCount: 2,
         padding: const EdgeInsets.all(16.0),
         childAspectRatio: 8.0 / 9.0,
-        children: <Widget>[
-          Card(
-            clipBehavior: Clip.antiAlias,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                AspectRatio(
-                  aspectRatio: 18.0 / 11.0,
-                  child: Image.asset('assets/diamond.png'),
-                ),
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text('Title'),
-                      const SizedBox(height: 8.0),
-                      Text('Secondary Text'),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
+        children: _buildGridCards(context)
       ),
       // TODO: Set resizeToAvoidBottomInset (101)
       resizeToAvoidBottomInset: false,
     );
+  }
+
+  // Make a collection of cards
+  List<Card> _buildGridCards(BuildContext context){
+    List<Product> products = ProductsRepository.loadProducts(Category.all);
+
+    if (products.isEmpty) {
+      return const <Card>[];
+    }
+
+    final ThemeData theme = Theme.of(context);
+    final NumberFormat formatter = NumberFormat.simpleCurrency(
+      locale: Localizations.localeOf(context).toString()
+    );
+
+    return products.map((product) {
+        return Card(
+          clipBehavior: Clip.antiAlias,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              AspectRatio(
+                aspectRatio: 18.0 / 11.0,
+                child: Image.asset(
+                  product.assetName,
+                  package: product.assetPackage,
+                  fit: BoxFit.fitWidth,
+                ),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        product.name,
+                        style: theme.textTheme.headline6,
+                        maxLines: 1,
+                      ),
+                      const SizedBox(height: 8.0),
+                      Text(
+                        formatter.format(product.price),
+                        style: theme.textTheme.subtitle2,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        );
+    }).toList();
   }
 }
